0e634e147df1fb0a5122007a0a83dc41fa98ea00
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-26 08:33:39 -0600

    closer to working, no err but content still doesn't load

--- virkshop/events/@setup_without_system_tools/electron_builder.deno.js
+++ virkshop/events/@setup_without_system_tools/electron_builder.deno.js
@@ -1,3 +1,4 @@
+import { FileSystem, glob } from "https://deno.land/x/quickr@0.6.72/main/file_system.js"
 import $ from "https://deno.land/x/dax@0.39.2/mod.ts"
 const $$ = (...args)=>$(...args).noThrow()
 export const deadlines = {
@@ -18,6 +19,25 @@ export const deadlines = {
                 Deno.exit(1)
             }
         }
+        
+        // let exists = false
+        // try {
+        //     isDirectory = Deno.lstatSync("/nix/var/nix/profiles/per-user/root/channels").isDirectory
+        // } catch (error) {
+            
+        // }
+        // if (!isDirectory) {
+        //     if (prompt(`For some reason, electron-builder needs "/nix/var/nix/profiles/per-user/root/channels" to exits\nYours does not.\nWould you like me to create it for you? (y/n)`,).startsWith("y")) {
+        //         console.log(`creating directory`)
+        //         await $$`sudo mkdir -p /nix/var/nix/profiles/per-user/root/channels`
+        //         console.log(`directory created`)
+        //     } else {
+        //         console.log(`Okay, please make the directory yourself (you'll need sudo)`)
+        //         Deno.exit(1)
+        //     }
+        // }
+        // await FileSystem.relativeLink({ existingItem: `${Deno.env.get("HOME")}/.local/state/nix/profiles/`,  newItem: `${Deno.env.get("HOME")}/.local/state/nix/profiles/profile`})
+        // `${Deno.getEnv("HOME")}/.local/state/nix/profiles/channels`
     },
     async beforeReadingSystemTools(virkshop) {
     },
